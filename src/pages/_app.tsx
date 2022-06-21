b938894788e208b4fc60a37ed313d814a7575dcf
deividbreda <deivid.breda02@gmail.com>
2022-06-20 23:51:53 -0300

    Criando componente _app com Header e Footer fixo

---
+++ src/pages/_app.tsx
@@ -0,0 +1,18 @@
+import { AppProps } from 'next/app'
+import { Footer } from '../components/Footer';
+import { Header } from '../components/Header';
+
+import '../styles/globals.scss';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return(
+    <>
+    <Header />
+    <Component {...pageProps} />
+    <Footer />
+    </>
+
+  )
+}
+
+export default MyApp
