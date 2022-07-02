ca4c57401ec31f8eacf7a9db1740a3b763018659
deividbreda <deivid.breda02@gmail.com>
2022-07-02 12:04:38 -0300

    Adicionando Context Providers na aplicação

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,20 @@
 import { AppProps } from 'next/app'
+import { SessionProvider } from 'next-auth/react';
 import { Footer } from '../components/Footer';
 import { Header } from '../components/Header';
 
 import '../styles/globals.scss';
+import { ModalProvider } from '../hook/useModal';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return(
-    <>
-    <Header />
-    <Component {...pageProps} />
-    <Footer />
-    </>
+    <SessionProvider>
+      <ModalProvider>
+        <Header />
+        <Component {...pageProps} />
+        <Footer />
+      </ModalProvider>
+    </SessionProvider>
 
   )
 }
