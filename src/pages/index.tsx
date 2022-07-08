6619d1389ee756c269a4ec906b15a83ec208e1bb
deividbreda <deivid.breda02@gmail.com>
2022-07-08 14:25:00 -0300

    Consumindo novos Types do PrismicCMS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,9 +5,13 @@ import { BotaoAssinar } from '../components/Homepage/BotaoAssinar'
 import { getPrismicClient } from '../services/prismic'
 import { stripe } from '../services/stripe'
 
-import { FaRegClock } from "react-icons/fa";
+import { BsAlarm } from "react-icons/bs";
+import { BiDish } from "react-icons/bi";
 
 import styles from './styles.module.scss'
+import Link from 'next/link'
+import { useSession } from 'next-auth/react'
+import { useModal } from '../hook/useModal'
 
 type Doces = {
   slug: string,
@@ -15,6 +19,7 @@ type Doces = {
   img: string,
   type: string,
   timer: number,
+  porcao: number,
 }
 
 type Massas = {
@@ -23,6 +28,7 @@ type Massas = {
   img: string,
   type: string,
   timer: number,
+  porcao: number,
 }
 
 type Carnes = {
@@ -44,6 +50,14 @@ interface HomeProps {
 }
 
 export default function Home({ produto, doces, massas, carnes }: HomeProps) {
+  const { data: session } = useSession();
+
+  const { openModal } = useModal();
+
+  function handleLogin() {
+    openModal();
+  }
+
   return(
     <>
       <Head> 
@@ -54,7 +68,7 @@ export default function Home({ produto, doces, massas, carnes }: HomeProps) {
         <div className={styles.featured}>
           <div className={styles.text}>
             <h1> As melhores <span> receitas </span> da internet estão aqui! </h1>
-            <p> Assine por apenas <b> {produto.preco} </b> e tenha acesso a todas as receitas! </p>
+            <p> Assine por apenas <b> {produto.preco} mensal </b> e tenha acesso a todas as receitas! </p>
             <BotaoAssinar />
           </div>
           <img src="/images/cartoonchef.png" alt="" />
@@ -65,14 +79,41 @@ export default function Home({ produto, doces, massas, carnes }: HomeProps) {
             <div className={styles.doces}>
               {doces.map(doce => (
                 <div key={doce.slug} className={styles.doce}>
-                  <a href="">
-                    <img src={doce.img} alt={doce.title} />
-                    <div className={styles.cover}>
-                      <span> {doce.type} </span>
-                      <h2> {doce.title} </h2>
-                      <time> <FaRegClock />  {doce.timer} min </time>
-                    </div>
-                  </a>
+                  {!session ?
+                    <a onClick={handleLogin}>
+                      <img src={doce.img} alt={doce.title} />
+                      <div className={styles.cover}>
+                        <Link href="/doces">
+                          <span> 
+                            {doce.type} 
+                          </span>
+                        </Link>
+                        <h2> {doce.title} </h2>
+                        <div className={styles.itens}>
+                          <p> <b> <BsAlarm /> </b> {doce.timer} min </p>
+                          <p className={styles.porcao}> <b> <BiDish /> </b> {doce.porcao} {doce.porcao == 1 ? "porção" : "porções"} </p>
+                        </div> 
+                      </div>
+                    </a>
+                  :
+                    <Link href={`/doces/${doce.slug}`}>
+                      <a>
+                        <img src={doce.img} alt={doce.title} />
+                        <div className={styles.cover}>
+                          <Link href="/doces">
+                            <span> 
+                              {doce.type} 
+                            </span>
+                          </Link>
+                          <h2> {doce.title} </h2>
+                          <div className={styles.itens}>
+                            <p> <b> <BsAlarm /> </b> {doce.timer} min </p>
+                            <p className={styles.porcao}> <b> <BiDish /> </b> {doce.porcao} {doce.porcao == 1 ? "porção" : "porções"} </p>
+                          </div> 
+                        </div>
+                      </a>
+                    </Link>
+                  }
                 </div>  
               ))}              
             </div>
@@ -82,14 +123,41 @@ export default function Home({ produto, doces, massas, carnes }: HomeProps) {
             <div className={styles.massas}>
               {massas.map(massa => (
                 <div key={massa.slug} className={styles.massa}>
-                  <a href="">
-                    <img src={massa.img} alt={massa.title} />
-                    <span> {massa.type} </span>
-                    <div className={styles.textoMassa}>
-                      <h1> {massa.title} </h1>
-                      <time> <FaRegClock /> {massa.timer} min </time>
-                    </div>
-                  </a>
+                  {!session ?
+                    <a onClick={handleLogin}>
+                      <img src={massa.img} alt={massa.title} />
+                      <Link href="/massas">
+                        <span> 
+                          {massa.type} 
+                        </span>
+                      </Link>
+                      <div className={styles.textoMassa}>
+                        <h1> {massa.title} </h1>
+                        <div className={styles.itens}>
+                          <p> <b> <BsAlarm /> </b> {massa.timer} min </p>
+                          <p className={styles.porcao}> <b> <BiDish /> </b> {massa.porcao} {massa.porcao == 1 ? "porção" : "porções"} </p>
+                        </div>                   
+                      </div>
+                    </a>
+                  :
+                    <Link href={`/massas/${massa.slug}`}>
+                      <a>
+                        <img src={massa.img} alt={massa.title} />
+                        <Link href="/massas">
+                          <span> 
+                            {massa.type} 
+                          </span>
+                        </Link>
+                        <div className={styles.textoMassa}>
+                          <h1> {massa.title} </h1>
+                          <div className={styles.itens}>
+                            <p> <b> <BsAlarm /> </b> {massa.timer} min </p>
+                            <p className={styles.porcao}> <b> <BiDish /> </b> {massa.porcao} {massa.porcao == 1 ? "porção" : "porções"} </p>
+                          </div>                   
+                        </div>
+                      </a>
+                    </Link>
+                  }
                 </div>
               ))}
             </div>
@@ -99,13 +167,33 @@ export default function Home({ produto, doces, massas, carnes }: HomeProps) {
             <div className={styles.carnes}>
               {carnes.map(carne => (
                 <div key={carne.slug} className={styles.carne}>
-                  <a href="">
-                    <img className={styles.img} src={carne.img} alt={carne.title} />
-                    <div className={styles.cover}>
-                      <span> {carne.type} </span>
-                      <h1> {carne.title} </h1> 
-                    </div>
-                  </a>
+                  {!session ?
+                    <a onClick={handleLogin}>
+                      <img className={styles.img} src={carne.img} alt={carne.title} />
+                      <div className={styles.cover}>
+                        <Link href="/carnes">
+                          <span> 
+                            {carne.type} 
+                          </span>
+                        </Link>
+                        <h1> {carne.title} </h1> 
+                      </div>
+                    </a>
+                  :
+                    <Link href={`/carnes/${carne.slug}`}>
+                      <a>
+                        <img className={styles.img} src={carne.img} alt={carne.title} />
+                        <div className={styles.cover}>
+                          <Link href="/carnes">
+                            <span> 
+                              {carne.type} 
+                            </span>
+                          </Link>
+                          <h1> {carne.title} </h1> 
+                        </div>
+                      </a>
+                    </Link>
+                  }
                 </div>
               ))}
             </div>
@@ -153,6 +241,7 @@ export const getStaticProps: GetStaticProps = async () => {
       img: massa.data.img.url,
       type: massa.data.type,
       timer: massa.data.timer,
+      porcao: massa.data.porcao,
     }
   })
 
@@ -167,10 +256,9 @@ export const getStaticProps: GetStaticProps = async () => {
       img: doce.data.img.url,
       type: doce.data.type,
       timer: doce.data.timer,
+      porcao: doce.data.porcao,
     };
   })
-
-  console.log(doces)
 
   return {
     props: {
